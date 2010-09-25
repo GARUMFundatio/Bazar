7169a79ae30e8178f93baab282429ca2afb2a7fc
Juantomas <juantomas@lared.es>
2010-09-25 20:23:49 +0200

    añadida la ultima version de jquery-ufs

--- bazar/app/controllers/user_sessions_controller.rb
+++ bazar/app/controllers/user_sessions_controller.rb
@@ -19,7 +19,9 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_to "/", :format => params[:format]
   end
+  
+  
 end
 
--- bazar/config/application.rb
+++ bazar/config/application.rb
@@ -38,5 +38,6 @@ module Bazar
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.action_view.javascript_expansions[:defaults] = ['jquery', 'jquery-ujs/src/rails']
   end
 end
