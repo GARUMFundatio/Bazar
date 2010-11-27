ddcb2864688ae147fc22081a735480f2519c456f
Juantomas <juantomas@lared.es>
2010-11-27 10:23:38 +0100

    borrado acts_as_taggable_on_steroids por acts-as-taggable-on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101125073025) do
+ActiveRecord::Schema.define(:version => 20101127091455) do
 
   create_table "confs", :force => true do |t|
     t.string   "nombre"
@@ -70,11 +70,14 @@ ActiveRecord::Schema.define(:version => 20101125073025) do
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
     t.datetime "created_at"
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
     t.string "name"
---
+++ install.sh
@@ -0,0 +1,2 @@
+
+echo "Instalación de Bazar" 
