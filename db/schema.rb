1505d805b3d16865bb26510604ede54bc2571573
Juantomas <juantomas@lared.es>
2010-12-05 12:09:48 +0100

    añadido ferret para busquedas por terminos y delayed_job para trabajos en background

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101127091455) do
+ActiveRecord::Schema.define(:version => 20101204192110) do
 
   create_table "confs", :force => true do |t|
     t.string   "nombre"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(:version => 20101127091455) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["locked_by"], :name => "delayed_jobs_locked_by"
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "empresas", :force => true do |t|
     t.integer  "user_id"
