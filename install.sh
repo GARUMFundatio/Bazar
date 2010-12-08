1677c972c1385628a44538f3094f5e1bdfb68a15
GARUM Fundatio <juantomas@lared.es>
2010-12-08 16:08:56 +0100

    creando la shell de instalacion

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
-
 echo "Instalación de Bazar" 
+
+echo "Compilando las dependencias en el directorio vendor" 
+
+bundle install --path vendor 
+
+echo "Haciendo el setup de bazar" 
+
+echo "Creando Directorio" 
+rake bazar:mkdir_dirs 
+
+echo "Permisos Directorio" 
+rake bazar:setup_dirs 
+
+echo "Creando usuarios en la BD" 
+rake bazar:create_users 
--- lib/tasks/setup.rb
+++ lib/tasks/setup.rb
@@ -2,17 +2,19 @@ namespace :bazar do
 
   desc "Estableciendo los permisos."
 
-  task :Setup_dirs do
+  task :mkdir_dirs do
+    FileUtils.mkdir_p File.join('.','public/system/ficheros')
+    FileUtils.mkdir_p File.join('.','tmp')
+    FileUtils.mkdir_p File.join('.','log')
+  end
+
+  task :setup_dirs do
     printf "Cambiando los permisos de los ficheros... ".ljust(75)
     FileUtils.chown 'www-data', 'www-data', File.join('.','tmp')
     FileUtils.chown 'www-data', 'www-data', File.join('.','log')
     FileUtils.chown_R 'www-data', 'www-data', File.join('.','public/system')
     FileUtils.chown 'www-data', 'www-data', File.join('config','database.yml')
     FileUtils.chown 'www-data', 'www-data', File.join('config','environment.rb')
-  end
-
-  task :mkdir_dirs do
-    FileUtils.mkdir_p File.join('.','public/system/ficheros')
   end
 
   desc "Crea los usuarios y los permisos para la base de datos"
