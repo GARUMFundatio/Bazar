174fb410dfb6f31d0edb75ebb0f2029608bbc92d
GARUM Fundatio <juantomas@lared.es>
2010-12-11 23:34:31 +0100

    inflector para role

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ inflect.singular /([aeiou])s([A-Z]|_)([a-z]+)([rlnd])es($)/, '\1\2\3\4\5'
 inflect.singular /([rlnd])es([A-Z]|_)([a-z]+)([aeiou])s($)/, '\1\2\3\4\5'
 
 inflect.irregular 'user', 'users'
+inflect.irregular 'rol', 'roles'
 inflect.irregular 'account', 'accounts'
 inflect.irregular 'password', 'passwords'
 inflect.irregular 'conf', 'confs'
