4a9cca8579da6a6958e6fdc9f107991115291019
GARUM Fundatio <juantomas@lared.es>
2011-01-29 17:31:43 +0100

    Resuelta la recuperación de la contraseña

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,36 +2,41 @@
 class PasswordResetsController < ApplicationController
   before_filter :load_user_using_perishable_token, :only => [ :edit, :update ]
   before_filter :require_no_user
-
+  
+  layout "bazar"
   def new
-    render :layout => false 
+         
   end
 
   def create
     @user = User.find_by_email(params[:email])
     if @user
-      Notifier.password_reset_instructions(@user).deliver
+      Notifier.password_reset_instructions(@user, Cluster.find(BZ_param("BazarId")).url.gsub("http://","")).deliver
       flash[:notice] = "Se han enviado instrucciones a tu dirección de correo"
       redirect_to "/"
     else
       flash[:error] = "Esta dirección no se corresponde con ningún usuario: #{params[:email]}"
-      render :action => :new, :layout => false
+      render :action => :new
     end
   end
 
   def edit
+    logger.debug "entro edit"
+    
+    render
   end
 
   def show
   end
 
   def update
-    @user.password = params[:password]
-    # Only if your are using password confirmation
-    # @user.password_confirmation = params[:password]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+   
     if @user.save
+      # logger.debug "actualizada la clave #{params.inspect} #{@user.inspect}"
       flash[:success] = "Tu contraseña se ha actualizado correctamente"
-      redirect_to @user
+      redirect_to "/home"
     else
       render :action => :edit
     end
@@ -41,10 +46,14 @@ class PasswordResetsController < ApplicationController
   private
 
   def load_user_using_perishable_token
-    @user = User.find_using_perishable_token(params[:id])
+    logger.debug "entro load_user_using_perishable_token"
+    @user = User.find_by_perishable_token(params[:id])
     unless @user
-      flash[:error] = "Lo siento, no encuentro tu cuenta."
+      logger.debug "no encuentro bazar #{params[:id]}"
+      flash[:error] = "Lo siento, no encuentro tu cuenta en bazar"
       redirect_to root_url
     end
+    logger.debug "salgo load_user_using_perishable_token"
+    
   end
 end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,13 +1,16 @@
 class Notifier < ActionMailer::Base
   # TODO: cambiar al host del bazar que corresponda 
   
-  default_url_options[:host] = "bazar.garumfundatio.org"
-  def password_reset_instructions(user)
+  default_url_options[:host] = "" # ApplicationController.BZ_param("BazarId")
+  
+  def password_reset_instructions(user, hostname)
+    default_url_options[:host] = hostname
     subject      "Bazar: Instrucciones para cambiar la contraseña"
-    from         "noresponder@bazar.com"
+    from         "noresponder@bazar.garumfundatio.org"
     recipients   user.email
     content_type "text/html"
     sent_on      Time.now
     body         :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
   end
+  
 end
