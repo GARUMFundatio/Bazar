0961e28bce7c9095d711bffd6a028dce2bb2d154
Juantomas Garum Fundatio <juanto@garumfundatio.org>
2011-02-12 02:15:43 +0100

    Explicando como funcionan las API e incluyendo ejemplos de uso

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -27,7 +27,7 @@ class ApiController < ApplicationController
     @info[:url] = Cluster.find(BZ_param("BazarId")).url
     @info[:empresas] = Bazarcms::Empresa.count_by_sql("select count(*) from empresas")
     @info[:consultas] = Bazarcms::Empresasconsulta.count_by_sql("select count(*) from empresasconsultas")
-    @info[:clusteractivos] = Cluster.count_by_sql("select count(*) from clusters where activo = 'S' ")
+    @info[:clustersactivos] = Cluster.count_by_sql("select count(*) from clusters where activo = 'S' ")
     
     respond_to do |format|
         format.html # info.html.erb
