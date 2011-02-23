69f12cb4439f564191c6c8256b2608c9345b02ac
Juantomas Garum Fundatio <juanto@garumfundatio.org>
2011-02-23 18:04:26 +0100

    homepage

--- app/models/actividad.rb
+++ app/models/actividad.rb
@@ -1,2 +1,17 @@
 class Actividad < ActiveRecord::Base
+  
+  def self.graba(texto, tipo)
+    
+    act = Actividad.new
+    
+    act.desc = texto 
+    act.bazar_id = ApplicationController.BZ_param("BazarId")
+    act.fecha = DateTime.now
+    act.user_id = user.current_user.id
+    act.local_id = 0
+    
+    act.save
+    
+  end
+  
 end
