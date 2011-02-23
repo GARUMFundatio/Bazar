51083732f08c56d0ea9e9bd60738e274d5502a05
Juantomas Garum Fundatio <juanto@garumfundatio.org>
2011-02-23 20:27:25 +0100

    homepage

--- app/models/actividad.rb
+++ app/models/actividad.rb
@@ -1,13 +1,13 @@
 class Actividad < ActiveRecord::Base
   
-  def self.graba(texto, tipo, cluster)
+  def self.graba(texto, tipo, cluster, user)
     
     act = Actividad.new
     
     act.desc = texto 
     act.bazar_id = cluster
     act.fecha = DateTime.now
-    act.user_id = current_user.id
+    act.user_id = user
     act.local_id = 0
     
     act.save
