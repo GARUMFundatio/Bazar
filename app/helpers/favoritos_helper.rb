c47ba962ee3903bfbfe894e8e7f8368cc79c0c8f
Fernando GARUMFundatio <fernando@lasindias.coop>
2011-02-26 15:05:59 -0300

    cambiado enlace a icono addfav.gif a addfav.png

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -5,7 +5,7 @@ module FavoritosHelper
     if (!fav.nil?)
       "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/favorito.png'></a></div>".html_safe
     else 
-      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/addfav.gif'></a></div>".html_safe
+      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/addfav.png'></a></div>".html_safe
     end
   end 
 end
