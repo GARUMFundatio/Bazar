a4a6ef2c6c21beec88dec1c6893787b97b6dcc40
Fernando GARUMFundatio <fernando@lasindias.coop>
2011-02-28 16:26:58 -0300

    agregado atributo title a los íconos de favoritos

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -3,9 +3,9 @@ module FavoritosHelper
   def helper_favo(bazar, empresa)
     fav = Favorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, current_user.id)
     if (!fav.nil?)
-      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/favorito.png'></a></div>".html_safe
+      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title="Quitar de Favoritos" onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/favorito.png'></a></div>".html_safe
     else 
-      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/addfav.png'></a></div>".html_safe
+      "<div id='favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title="Agregar a Favoritos" onclick='$(\"\#favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}\")'><img src='images/addfav.png'></a></div>".html_safe
     end
   end 
 end
