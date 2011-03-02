5f4446381a6e0df70f849fa09f864570356b72fd
Juantomas Garum Fundatio <juanto@garumfundatio.org>
2011-03-02 14:44:03 +0100

    resuelto problema de la home si estabas loggueado

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,30 @@ class HomeController < ApplicationController
   layout "bazar"
   before_filter :require_user, :only => [:index]
   
+  # homepage de usuario 
+  
   def index
     
   end
 
-  def home
+  # homepage de bazar 
+  
+  def home    
+
+    # compruebo si está logueado. 
+    
+    if !current_user.nil?
+      # le mandamos a la página de usuario
+      
+      render :action => "index"
+    end
+    
+
+    # TODO: comprobar si esto sobra 
+    
     @ultimas = Bazarcms::Empresa.ultimascreadas
     @actualizadas = Bazarcms::Empresa.ultimasactualizadas
+
   end 
   
 end
