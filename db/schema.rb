39c504f64663ffcd9586a02baaa2a9e31bcc1a60
Webmaster Server <webmaster@garumfundatio.org>
2011-04-23 22:33:43 +0200

    eliminando db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -306,15 +306,6 @@ ActiveRecord::Schema.define(:version => 20110422105955) do
 
   add_index "perfiles", ["codigo"], :name => "index_perfiles_on_codigo"
 
-  create_table "perfiles_copy", :force => true do |t|
-    t.string  "codigo"
-    t.string  "desc"
-    t.integer "nivel"
-    t.text    "ayuda"
-  end
-
-  add_index "perfiles_copy", ["codigo"], :name => "index_perfiles_on_codigo"
-
   create_table "roles", :force => true do |t|
     t.string   "rol"
     t.datetime "created_at"
