ab499b6253a908c9cf36fdb67591b385b6c3228e
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-04-24 02:02:02 +0200

    Generando el fichero sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,12 +11,12 @@ SitemapGenerator::Sitemap.add_links do |sitemap|
 
   Bazarcms::Oferta.find_each do |oferta|
     puts oferta.inspect
-    sitemap.add "/bazarcsm/ofertas/#{oferta.id}?bazar_id=#{oferta.bazar_id}", :priority => 0.7, :changefreq => 'daily',:lastmod => oferta.fecha
+    sitemap.add "/bazarcms/ofertas/#{oferta.id}?bazar_id=#{oferta.bazar_id}", :priority => 0.7, :changefreq => 'daily',:lastmod => oferta.fecha
   end
 
   Bazarcms::Empresa.find_each do |empresa|
     puts empresa.inspect
-    sitemap.add "/bazarcsm/empresas/#{empresa.id}?bazar_id=#{Conf.find_by_nombre("BazarId").valor}", :priority => 0.7, :changefreq => 'daily',:lastmod => empresa.updated_at
+    sitemap.add "/bazarcms/empresas/#{empresa.id}?bazar_id=#{Conf.find_by_nombre("BazarId").valor}", :priority => 0.7, :changefreq => 'daily',:lastmod => empresa.updated_at
   end
   
 end
