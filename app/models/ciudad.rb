5be65353a4f53ba931b09939f623c36a31ac032e
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-04-24 14:27:22 +0200

    Usuario: no se puede cambiar el login

--- app/models/ciudad.rb
+++ app/models/ciudad.rb
@@ -3,7 +3,9 @@ class Ciudad < ActiveRecord::Base
   belongs_to :pais
   has_many :ubicaciones
   default_scope :order => 'descripcion'
-
+  
+  has_friendly_id :descripcion, :use_slug => true, :approximate_ascii => true
+  
   def to_s;self.descripcion;end
 
 end
