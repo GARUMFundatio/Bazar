e70180631578b267c55e11858d8c2c12cd95050e
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-04-24 11:31:52 +0200

    Sitemaps: no incluimos el directorio garum

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,13 +5,13 @@ SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.add_links do |sitemap|
 
-  Cluster.find_each do |cluster|
-    sitemap.add "/clusters/#{cluster.slug.name}", :priority => 0.7, :changefreq => 'daily',:lastmod => cluster.updated_at
+  Cluster.where ('id <> 1') do |cluster|
+    sitemap.add "/clusters/#{cluster.slug.name}", :priority => 0.4, :changefreq => 'daily',:lastmod => cluster.updated_at
   end
 
   Bazarcms::Oferta.find_each do |oferta|
     puts oferta.inspect
-    sitemap.add "/bazarcms/ofertas/#{oferta.id}?bazar_id=#{oferta.bazar_id}", :priority => 0.7, :changefreq => 'daily',:lastmod => oferta.fecha
+    sitemap.add "/bazarcms/ofertas/#{oferta.id}?bazar_id=#{oferta.bazar_id}", :priority => 0.9, :changefreq => 'daily',:lastmod => oferta.fecha
   end
 
   Bazarcms::Empresa.find_each do |empresa|
