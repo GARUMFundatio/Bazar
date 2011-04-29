6af6489225aa0b411fed2910b6e9bfa28ce49f4a
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-04-29 17:41:46 +0200

    Notificación de errores del sistema vía email

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,4 @@
-Bazar::Application.config.middleware.use ExceptionNotifier,
+Bazar::Application.config.middleware.use ::ExceptionNotifier,
   :email_prefix => "[Bazar Garum] ",
   :sender_address => %{"Notifier" <juantomas@geofun.es>},
   :exception_recipients => %w{juantomas.garcia@gmail.com}
