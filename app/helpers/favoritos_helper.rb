8e08d361f5708191149d1f03ca1563088db047fa
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-06-23 23:23:20 +0200

    pasando el user id en remoto

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -8,4 +8,14 @@ module FavoritosHelper
       "<div id='#{pre}-favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title='Agregar a Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
     end
   end 
+  
+  def helper_favo2(bazar, empresa, nombre, pre, user)
+    fav = Favorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, user)
+    if (!fav.nil?)
+      "<div id='#{pre}-favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title='Quitar de Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
+    else 
+      "<div id='#{pre}-favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title='Agregar a Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+    end
+  end 
+  
 end
