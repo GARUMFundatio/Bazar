c003e86e1c0b925a953d01ffd6555768ef190f71
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-06-25 22:24:05 +0200

    favoritos: Resolviendo el error si ya ha sido borrado un favorito

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -3,9 +3,9 @@ module FavoritosHelper
   def helper_favo(bazar, empresa, nombre, pre)
     fav = Favorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, current_user.id)
     if (!fav.nil?)
-      "<div class='#{pre}-favo#{bazar}-#{empresa}'><a href='#' title='Quitar de Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
+      "<div class='#{pre}-favo#{bazar}-#{empresa}'><a href='#' title='Quitar de Favoritos' onclick='$(\"\.#{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
     else 
-      "<div class='#{pre}-favo#{bazar}-#{empresa}'><a href='#' title='Agregar a Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+      "<div class='#{pre}-favo#{bazar}-#{empresa}'><a href='#' title='Agregar a Favoritos' onclick='$(\"\.#{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
     end
   end 
   
@@ -14,7 +14,7 @@ module FavoritosHelper
     # if (!fav.nil?)
     #  "<div id='#{pre}-favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title='Quitar de Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
     # else 
-      "<div class='#{pre}-favo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+      "<div class='#{pre}-favo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\"\.#{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
     #end
   end 
   
