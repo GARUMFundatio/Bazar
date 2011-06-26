4c6edba5955d79c82d3815cd3cbcf3abba433824
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-06-26 17:42:33 +0200

    Favoritos Ofertas: incluyendolo en los dashboard

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -1,21 +1,38 @@
 module FavoritosHelper
   
   def helper_favo(bazar, empresa, nombre, pre)
+
     fav = Favorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, current_user.id)
     if (!fav.nil?)
       "<div class='favoritos favo#{bazar}-#{empresa}'><a href='#' title='Quitar de Favoritos' onclick='$(\".favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
     else 
       "<div class='favoritos favo#{bazar}-#{empresa}'><a href='#' title='Agregar a Favoritos' onclick='$(\".favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
     end
+
   end 
   
   def helper_favo2(bazar, empresa, nombre, pre, user)
-    # fav = Favorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, user)
-    # if (!fav.nil?)
-    #  "<div id='#{pre}-favo#{bazar}-#{empresa}' class='favoritos'><a href='#' title='Quitar de Favoritos' onclick='$(\"\##{pre}-favo#{bazar}-#{empresa}\").load(\"/favorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
-    # else 
-      "<div class='favoritos favo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\".favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
-    #end
+
+    "<div class='favoritos favo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\".favo#{bazar}-#{empresa}\").load(\"/favorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+
   end 
+  
+  def helper_favoofe(bazar, empresa, nombre, pre)
+
+    fav = Bazarcms::Ofertasfavorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, current_user.id)
+    if (!fav.nil?)
+      "<div class='favoritos favoo#{bazar}-#{empresa}'><a href='#' title='Quitar de Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
+    else 
+      "<div class='favoritos favoo#{bazar}-#{empresa}'><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+    end
+
+  end 
+  
+  def helper_favoofe2(bazar, empresa, nombre, pre, user)
+
+    "<div class='favoritos favoo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+
+  end 
+  
   
 end
