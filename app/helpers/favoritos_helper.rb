58fdbfe33c0e9da85307d4adea77bf0fd6920258
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-06-26 21:49:56 +0200

    Favoritos Ofertas: reorganizando los helpers de favoritos

--- app/helpers/favoritos_helper.rb
+++ app/helpers/favoritos_helper.rb
@@ -17,20 +17,20 @@ module FavoritosHelper
 
   end 
   
-  def helper_favoofe(bazar, empresa, nombre, pre)
+  def helper_favoofe(bazar, empresa, nombre, oferta, nombreo)
 
-    fav = Bazarcms::Ofertasfavorito.find_by_bazar_id_and_empresa_id_and_user_id(bazar, empresa, current_user.id)
+    fav = Bazarcms::Ofertasfavorito.find_by_bazar_id_and_oferta_id_and_user_id(bazar, oferta, current_user.id)
     if (!fav.nil?)
-      "<div class='favoritos favoo#{bazar}-#{empresa}'><a href='#' title='Quitar de Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/delfav?bazar=#{bazar}&empresa=#{empresa}&pre=#{pre}\")'><img src='/images/favorito.png'></a></div>".html_safe
+      "<div class='favoritos favoo#{bazar}-#{oferta}'><a href='#' title='Quitar de Favoritos' onclick='$(\".favoo#{bazar}-#{oferta}\").load(\"/ofertasfavorito/delfav?bazar=#{bazar}&oferta=#{oferta}\")'><img src='/images/favorito.png'></a></div>".html_safe
     else 
-      "<div class='favoritos favoo#{bazar}-#{empresa}'><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+      "<div class='favoritos favoo#{bazar}-#{oferta}'><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{oferta}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&oferta=#{oferta}&nombre_oferta=#{nombreo.gsub(' ','_')}\")'><img src='/images/addfav.png'></a></div>".html_safe
     end
 
   end 
   
-  def helper_favoofe2(bazar, empresa, nombre, pre, user)
+  def helper_favoofe2(bazar, empresa, nombre, oferta, nombreo, user)
 
-    "<div class='favoritos favoo#{bazar}-#{empresa}' ><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{empresa}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&pre=#{pre}\")'><img src='/images/addfav.png'></a></div>".html_safe
+    "<div class='favoritos favoo#{bazar}-#{oferta}' ><a href='#' title='Agregar a Favoritos' onclick='$(\".favoo#{bazar}-#{oferta}\").load(\"/ofertasfavorito/addfav?bazar=#{bazar}&empresa=#{empresa}&nombre_empresa=#{nombre.gsub(' ','_')}&oferta=#{oferta}&nombre_oferta=#{nombreo.gsub(' ','_')}\")'><img src='/images/addfav.png'></a></div>".html_safe
 
   end 
   
