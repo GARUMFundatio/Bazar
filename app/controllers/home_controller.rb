2039993947d9119c2582c67e263370e385e764fd
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-07-16 13:17:36 +0200

    Users: incluyendo más información de los usuarios: cuantas veces se ha entrado y cuando fué la última vez

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,9 +32,8 @@ class HomeController < ApplicationController
   end 
 
   def datos 
-    # dc = Dalli::Client.new('localhost')
-    # dc.set('abc', 123)
-    # value = dc.get('abc')
+
+
   end 
 
   def open 
