6009c7c316827405c708cd0b02366f7519bb290d
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2011-07-28 18:12:44 +0200

    Mailer: Versión html de la confirmación del registro

--- app/mailers/bazar_mailer.rb
+++ app/mailers/bazar_mailer.rb
@@ -7,8 +7,9 @@ class BazarMailer < ActionMailer::Base
     @username = user.login
     @userid = user.id
     @url = Cluster.find_by_id(Conf.find_by_nombre('BazarId').valor).url
-    
-    mail(:to => user.email, :subject => "Se ha registrado en Bazar")  
+    @bazar = Cluster.find_by_id(Conf.find_by_nombre('Titular').valor).url
+
+    mail(:to => user.email, :subject => "[#{@bazar}] Se ha registrado en el Bazar")  
     
   end
   
