230aeaaec6d70b4ba42deb82cba54a9d8e9c4359
Manuel Ortega <manuel@elarte.coop>
2012-03-19 17:12:44 +0100

    Ajustes i18n y traducción de ficha de oferta  fichaoferta.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Bazar::Application.configure do
   
   config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
   
-  config.i18n.default_locale = :es
+  config.i18n.default_locale = :eo
 
   
   
