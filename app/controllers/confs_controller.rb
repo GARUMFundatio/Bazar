b448271bdcd72dedb9d916dda4a1f61d77f56531
Juantomas - GARUM Fundatio <juanto@garumfundatio.org>
2012-04-11 20:28:59 +0200

    configuracion: solo la pueden ver los admins

--- app/controllers/confs_controller.rb
+++ app/controllers/confs_controller.rb
@@ -8,15 +8,12 @@ class ConfsController < ApplicationController
   def index
     @confs = Conf.order(:grupo_id)
     @grupos = Gruposconf.all
-    
-          
-    respond_to do |format|
-      if (current_user_is_admin )
-        format.html # index.html.erb
-      else 
-        format.html (redirect_to "/home")
-      end 
-    end
+        
+    if (current_user_is_admin )
+      render 
+    else 
+      redirect_to "/home"
+    end 
   end
 
   def bulk
@@ -25,81 +22,71 @@ class ConfsController < ApplicationController
     
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @confs }
     end
   end
   
-  # GET /confs/1
-  # GET /confs/1.xml
   def show
     @conf = Conf.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @conf }
+      if (current_user_is_admin )
+        format.html # show.html.erb
+      else 
+        format.html { redirect_to "/home" }
+      end 
     end
   end
 
-  # GET /confs/new
-  # GET /confs/new.xml
   def new
-    @conf = Conf.new
-    @grupos = Gruposconf.all
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @conf }
-    end
+    redirect_to "/home"
   end
 
-  # GET /confs/1/edit
   def edit
     @conf = Conf.find(params[:id])
     @grupos = Gruposconf.all
+    
+    if (current_user_is_admin )
+      render
+    else 
+      redirect_to "/home"
+    end 
   end
 
-  # POST /confs
-  # POST /confs.xml
   def create
     @conf = Conf.new(params[:conf])
     
     respond_to do |format|
-      if @conf.save
-        format.html { redirect_to(@conf, :notice => 'Parametro actualizado correctamente.') }
-        format.xml  { render :xml => @conf, :status => :created, :location => @conf }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @conf.errors, :status => :unprocessable_entity }
-      end
+      if (current_user_is_admin )
+        if @conf.save
+          format.html { redirect_to(@conf, :notice => 'Parametro actualizado correctamente.') }
+        else
+          format.html { render :action => "new" }
+        end
+      else 
+        format.html { redirect_to "/home" }
+      end 
     end
   end
 
-  # PUT /confs/1
-  # PUT /confs/1.xml
   def update
     @conf = Conf.find(params[:id])
-    
-    # @conf.grupo_id = params[:grupo_id][:id]
-    
+        
     respond_to do |format|
-      if @conf.update_attributes(params[:conf])
-        format.html { redirect_to(@conf, :notice => 'Parametro actualizado correctamente') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @conf.errors, :status => :unprocessable_entity }
-      end
+      if (current_user_is_admin )
+      
+        if @conf.update_attributes(params[:conf])
+          format.html { redirect_to(@conf, :notice => 'Parametro actualizado correctamente') }
+        else
+          format.html { render :action => "edit" }
+        end
+      else 
+        format.html { redirect_to "/home" }
+      end 
     end
   end
 
-  # DELETE /confs/1
-  # DELETE /confs/1.xml
   def destroy
-    @conf = Conf.find(params[:id])
-    @conf.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(confs_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to "/home"
   end
+  
 end
